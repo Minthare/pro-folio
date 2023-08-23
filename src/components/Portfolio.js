addf70f34a331c3900cfd4cf75ddebe9b7fa5d93
Joshua Pierre <119153351+Minthare@users.noreply.github.com>
2023-08-23 13:53:18 -0400

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Project from './Project';
 
 import '../styles/Portfolio.css';
-import password from './password.png';
-import work from './work.png';
+
 
 const Portfolio = () => {
   
