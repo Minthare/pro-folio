c858c58d32f1b193502d617ee7ef7533e7ee201f
Joshua Pierre <119153351+Minthare@users.noreply.github.com>
2023-08-23 13:57:53 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,26 +8,28 @@ const Header = ({ currentSection, setCurrentSection }) => {
       <div className="navigation">
         <h3>Joshua Pierre</h3>
         <a
-          
+          href ="/"
           className={currentSection === 'About' ? 'active' : ''}
           onClick={() => setCurrentSection('About')}
         >
           About
         </a>
         <a
+          href ="/"
           className={currentSection === 'Portfolio' ? 'active' : ''}
           onClick={() => setCurrentSection('Portfolio')}
         >
           Portfolio
         </a>
         <a
+          href ="/"
           className={currentSection === 'Contact' ? 'active' : ''}
           onClick={() => setCurrentSection('Contact')}
         >
           Contact
         </a>
         <a
-      
+      href ="/"
           className={currentSection === 'Resume' ? 'active' : ''}
           onClick={() => setCurrentSection('Resume')}
         >
